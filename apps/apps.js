f02c31aceccc34a06c560e84e8c65c93599f6c77
Mustafa Erol <codeeleven@codeberg.org>
2026-09-11 22:40:33 +0300

    added new system and xterm.js wasm apps

--- apps/apps.js
+++ apps/apps.js
@@ -3,5 +3,10 @@ const apps = {
         uuid: "b84668d5-c920-42b1-878d-c5268d0fad29",
         name: "About This Wallpaper",
         icon: "aperture-svgrepo-com.svg"
+    },
+    "@terminal": {
+        uuid: "db9155c5-4f10-4548-8c0c-d2119a63a483",
+        name: "Terminal",
+        icon: "terminal-svgrepo-com.svg"
     }
 };
